fa25bfc962df6e1e5299f4e954cebdb2302f147a
Chezlin Benson <102852227+chezlinbenson@users.noreply.github.com>
2022-04-30 20:37:48 +0200

    Added update carousel and daynight buttons

--- script.js
+++ script.js
@@ -1,14 +1,38 @@
-document.getElementById('changebtn').addEventListener('click', (event) => {
-  let element_changeimg = document.getElementById('changeimg');
-  element_changeimg.setAttribute("src", 'https://images.unsplash.com/photo-1534670007418-fbb7f6cf32c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NTh8fHV4JTIwdWklMjBkZXNpZ258ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60');
-  event.target.innerText = 'Refresh Page';
-  event.target.style.backgroundColor = '#ffff00';
-  event.target.style.color = '#000000';
+var images, numbers;
+
+
+images = ['https://images.unsplash.com/photo-1558655146-d09347e92766?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=464&q=80','https://images.unsplash.com/photo-1534670007418-fbb7f6cf32c3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80', 'https://images.unsplash.com/photo-1507238691740-187a5b1d37b8?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=455&q=80', 'https://images.unsplash.com/photo-1498758536662-35b82cd15e29?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=388&q=80'];
+numbers = [1, 2, 3, 4];
+
+let element_number = document.getElementById('number');
+element_number.innerText = numbers[0];
+let element_images = document.getElementById('images');
+element_images.setAttribute("src", images[0]);
+
+
+document.getElementById('next').addEventListener('click', (event) => {
+  numbers.push(numbers.shift());
+  images.push(images.shift());
+  let element_number2 = document.getElementById('number');
+  element_number2.innerText = numbers[0];
+  let element_images2 = document.getElementById('images');
+  element_images2.setAttribute("src", images[0]);
+
 });
+
+document.getElementById('previous').addEventListener('click', (event) => {
+  numbers.unshift(numbers.pop());
+  images.unshift(images.pop());
+  let element_images3 = document.getElementById('images');
+  element_images3.setAttribute("src", images[0]);
+  let element_number3 = document.getElementById('number');
+  element_number3.innerText = numbers[0];
+});
+
 
 document.getElementById('nightmode').addEventListener('click', (event) => {
   let element_nightmode = document.getElementById('pagemode');
-  element_nightmode.style.backgroundColor = '#1d1b1f';
+  element_nightmode.style.backgroundColor = '#1f1c13';
   element_nightmode.style.color = '#ffffff';
 });
 
@@ -17,72 +41,3 @@ document.getElementById('daymode').addEventListener('click', (event) => {
   element_daymode.style.backgroundColor = '#ffffff';
   element_daymode.style.color = 'black';
 });
-
-/* =================
-  TESTS, LOOK AT THESE
-  Reading tests will always help you discover your requirements.
-  You can make this window bigger. 
-   ===================
-*/
-
-const {
-  core: { test, expect, run },
-  prettify
-} = window.jestLite;
-
-/* =================
-  FIND ELEMENTS
-  These are all the elements we will look for.
-   ===================
-*/
-const getHeader = document.querySelectorAll("header"),
-  getH1 = document.querySelectorAll("h1"),
-  getSiteHeader = document.querySelectorAll(".c-site-header"),
-  getAria = document.querySelectorAll('nav[aria-label="Main Site Links."]'),
-  getMain = document.querySelectorAll("main"),
-  getFooter = document.querySelectorAll("footer"),
-  getSiteFooter = document.querySelectorAll(".c-site-footer"),
-  getIFrame = document.querySelectorAll("iframe"),
-  getImage = document.querySelectorAll("img"),
-  getWords = document.body.innerText;
-
-/* =================
-   ASSERTIONS 
-   These are the things we check are true about your page.
-   Read and update your HTML to discover the requirements.
-   The tests will run every time you update your code.
-   ===================
-*/
-test("There is at least one header element", () => {
-  expect(getHeader.length).toBeGreaterThanOrEqual(1);
-});
-test("There is at least one h1", () => {
-  expect(getH1.length).toBeGreaterThanOrEqual(1);
-});
-test("There is only one header element with the class c-site-header", () => {
-  expect(getSiteHeader.length).toBe(1);
-});
-test("There is a nav element with an aria-label of Main Site Links.", () => {
-  expect(getAria.length).toBeGreaterThanOrEqual(1);
-});
-test("There is only one main element", () => {
-  expect(getMain.length).toBe(1);
-});
-test("There is at least one footer element", () => {
-  expect(getFooter.length).toBeGreaterThanOrEqual(1);
-});
-test("There is only one footer element with the class c-site-footer", () => {
-  expect(getSiteFooter.length).toBe(1);
-});
-test("There is embedded video", () => {
-  expect(getIFrame.length).toBeGreaterThanOrEqual(1);
-});
-test("There is at least one image", () => {
-  expect(getImage.length).toBeGreaterThanOrEqual(1);
-});
-test("There are at least 500 words on the page", () => {
-  expect(getWords.length).toBeGreaterThanOrEqual(500);
-});
-
-const console = document.getElementById("tests");
-prettify.toHTML(run(), console);
